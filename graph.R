41793ad8cf41ba58fae40a863d094543c2f6f977
rozek1app2 <50049886+rozek1app2@users.noreply.github.com>
2020-07-24 22:51:40 +0200

    Update graph.R

--- graph.R
+++ graph.R
@@ -5,37 +5,16 @@ library(ggplot2)
 library(ggrepel)
 library(tidyr)
 
-
-graph <-function(ds,ae){
-  # call this to draw the graph
-  
-  # create new column which contains info about whether given harvest contained edited articles or not
-  merge <- add_row(ds %>% add_column(contains_edited=TRUE), 
-                   ds %>% filter(!page_id %in% ae$page_id) %>%
-                    add_column(contains_edited=FALSE)
-                   ) 
-  
-  # calculate n of occurences for each grouped variable
-  plot_data <- merge %>% group_by(date_harvested, contains_edited) %>% count(date_harvested)
-  
-  # draw plot
-  return(ggplot(data = plot_data, aes(x=date_harvested, y = n, color=contains_edited)) + 
-    geom_line() +
-    scale_color_discrete(name = "Vysvětlivky", labels = c("Bez editací", "Všechny odkazy")) +
-    labs(title="Vývoj počtu odkazů", x = "datum", y = "Počet odkazů")
-  )
-}
-
 graph_labels <- function(ds,ae){
-  # call this to draw the graph
+  # funkce pro kreslení grafu
   
-  # create new column which contains info about whether given harvest contained edited articles or not
+
   merge <- add_row(ds %>% add_column(contains_edited=TRUE), 
-                   ds %>% filter(!page_id %in% ae$page_id) %>%
-                     add_column(contains_edited=FALSE)
+                   ds %>% filter(!page_id %in% ae$page_id | date_harvested > "2020-04-20" & date_harvested < "2020-05-10") %>% 
+                          add_column(contains_edited=FALSE)
   ) 
   
-  # calculate n of occurences for each grouped variable
+
   plot_comments <- merge %>% group_by(date_harvested, contains_edited) %>% count(date_harvested) %>% add_column(comment=NA)
   plot_comments <- plot_comments %>% mutate(comment=replace(comment, date_harvested == "2020-05-08" & contains_edited==TRUE, "První kontakt s wikipedisty"))
   plot_comments <- plot_comments %>% mutate(comment=replace(comment, date_harvested == "2020-06-11" & contains_edited==TRUE, "Poslední rozhovor"))
@@ -44,12 +23,10 @@ graph_labels <- function(ds,ae){
   
   ggplot(data = plot_comments, aes(x=date_harvested, y = n)) + 
     geom_area(aes(x=date_harvested, y = n, fill = factor(contains_edited, levels = c(TRUE, FALSE))), position = "identity") +
-    # geom_line(aes(x=date_harvested, y = n, color = factor(contains_edited, levels = c(TRUE, FALSE))), name = "legenda") + 
     scale_fill_discrete(name = "Legenda", labels = c("Veškeré odkazy", "Odkazy bez provedených editací")) + 
     scale_x_date(date_breaks = "1 month", date_labels = "%m", labels = c(1, 2, 3, 4, 5, 6, 7)) + 
     geom_point(data = drop_na(plot_comments),aes(x=date_harvested, y = n)) + 
     geom_label_repel(data = drop_na(plot_comments),aes(x=date_harvested, y = n, label = comment),  direction = "x", nudge_y = 15) +
-    # scale_y_continuous(limits = c(0, 175), breaks = seq(0, 175, 25)) +
     labs(title="Monitorování zpětných odkazů ze Wikipedie do Sociologické encyklopedie", x = "Časové rozmezí (12. 3. 2020 - 19. 7. 2020)", y = "Počet odkazů")  + 
     coord_cartesian(ylim = c(75, 175))
 }
