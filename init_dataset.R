76c44232e1c5bace4e5052338f93502645070580
rozek1app2 <50049886+rozek1app2@users.noreply.github.com>
2020-07-24 22:54:13 +0200

    Update init_dataset.R

--- init_dataset.R
+++ init_dataset.R
@@ -3,7 +3,6 @@ library(tibble)
 library(dplyr)
 
 init_dataset <- function(source_folder){
-  # takes source folder name and merges all files inside it into a single dataset
   filelist <- list.files(source_folder, full.names = TRUE)
   dataset <- tibble()
   for (file in filelist){
@@ -14,28 +13,25 @@ init_dataset <- function(source_folder){
     new_data <- new_data %>% add_column(date_harvested = rep(substr(file,16,26), times=nrow(new_data)), )
     dataset <- bind_rows(dataset, new_data)
   }
-  
+
   dataset$date_harvested <- as.Date(dataset$date_harvested, "%Y-%m-%d")
   
-
+  
   dataset <- dataset %>% filter(!grepl("^Diskuse s wikipedistou:", title))
   dataset <- dataset %>% filter(!grepl("^Diskuse ke kategorii", title))
   dataset <- dataset %>% filter(!grepl("^Diskuse k portálu:", title))
   dataset <- dataset %>% filter(!grepl("^Diskuse:", title))
   dataset <- dataset %>% filter(!grepl("^Wikipedie:", title))
-  dataset <- dataset %>% filter(!grepl("^Wikipedista:", title))
   
-  # returns dataset, use variable_name <- init_dataset("./source") to add into workspace
+  
   return(dataset)
 }
 
 init_articles_edited <-function(){
-  # load the edited articles from csv
   articles_edited <- read_csv("rstudio_import/articles_edited_list.csv")
   return(articles_edited)
 }
 
 add_page_id_to_ae <- function(ae,ds){
-  # add page id's using title provided in dataset
   return(left_join(ae,ds %>% distinct(title,.keep_all = TRUE), "title") %>% select(page_id,title, wiki_url=url.x, soc_url=url.y, date_harvested))
 }
